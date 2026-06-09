1ef61f7e655abb0eff690bd03599f9fe8c315ece
guruabhay25 <guptaabhay2727@gmail.com>
2026-06-09 15:00:13 +0530

    Props project Done

--- 04-props-projects/src/components/Card.jsx
+++ 04-props-projects/src/components/Card.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props);
+    
   return (
     <div className="card">
         <div className="top">
-          <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
+          <img src={props.brandLogo} alt="" />
           <button>Save</button>
         </div>
         <div className="centre">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/Ux designer</h2> 
+          <h3>{props.company} <span>5 days ago</span></h3>
+          <h2>{props.post}</h2> 
           <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag2}</h4>
+            <h4>{props.tag1}</h4>
           </div>         
         </div>
         <div className="bottom">
           <div>
-            <h3>$120/hr</h3>
-            <p>Mumbai,India</p>
+            <h3>{props.pay}</h3>
+            <p>{props.location}</p>
           </div>
           <button>Apply Now</button>
         </div>
